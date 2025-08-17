cc4b29b05964e565eb6064cac6a1065f639c2e15
Yumanyer <manuelpisoni09@gmail.com>
2025-08-17 19:19:05 -0300

    agregue dirname para concetar con front y npm run app

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import express from "express"
+import path from "path"
 import { dataBase } from "./config/connectDB.config.js"
 import {config} from "./config/env.config.js"
 import userRoutes from "./routes/user.routes.js"
 import matterRouter from "./routes/matterRaw.routes.js"
+import getDirname from "../dirname.js"
 
 
 // CONFIG SERVER
 const app = express()
-const PORT = process.env.PORT || config.port       
+const PORT = process.env.PORT || config.port 
+const publicPath = path.join(getDirname(), '../frontend/public')
 
 // config de express
 app.use(express.json() )
 app.use(express.urlencoded({ extended: true }))
 
-// CONEXION A LA postgreSQL
+// CONFIG DE FRONT 
+app.use(express.static(publicPath))
+
+ // CONEXION A LA postgreSQL
 dataBase.query("SELECT NOW()")
 .then(()=>{
     console.log("Base de datos conectada")
@@ -34,12 +40,12 @@ async function InitApp(){
 
 
         app.get("/",(req,res)=>{
-            res.send("server prendido")
+            res.sendFile(path.join(publicPath,"home.html"))
         })
 
          // UNA VEZ CONECTADA LA DB INICIO LA APP
-        app.listen(config.port,()=>{
-        console.log(`Escuchando a http://localhost:${config.port}/`)})
+        app.listen(config.port, ()=>{
+        console.log(`Escuchando a http://localhost:${config.port} ` )})
 
     } catch (error) {
         console.error('No se pudo conectar a la base de datos:', error);
